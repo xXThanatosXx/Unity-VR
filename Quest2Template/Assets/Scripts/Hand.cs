743ed49e3baec3450ccc1686fa36033333715489
Shadowelec1986 <38089172+Shadowelec1986@users.noreply.github.com>
2022-10-07 13:17:46 -0500

    Templeate

---
+++ Quest2Template/Assets/Scripts/Hand.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class Hand : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed;
+    Animator animator;
+    SkinnedMeshRenderer mesh;
+    private float gripTarget;
+    private float triggerTarget;
+    private float gripCurrent;
+    private float triggerCurrent;
+    private string animatorGripParam = "Grip";
+    private string animatorTriggerParam = "Trigger";
+
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        mesh = GetComponentInChildren<SkinnedMeshRenderer>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AnimateHand();
+    }
+
+    internal void GetGrip(float v)
+    {
+        gripTarget = v;
+    }
+
+    internal void SetTrigger(float v)
+    {
+        triggerTarget = v;
+    }
+    void AnimateHand()
+    {
+        if (gripCurrent != gripTarget)
+        {
+            gripCurrent = Mathf.MoveTowards(gripCurrent, gripTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorGripParam, gripCurrent);
+        
+        }
+        if (triggerCurrent != triggerTarget)
+        {
+            triggerCurrent = Mathf.MoveTowards(triggerCurrent, triggerTarget, Time.deltaTime * speed);
+            animator.SetFloat(animatorTriggerParam, triggerCurrent);
+        
+        }
+
+    
+    }
+
+    public void ToggleVisibility()
+    {
+        mesh.enabled = !mesh.enabled;
+    }
+}
